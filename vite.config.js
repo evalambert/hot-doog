6715375e8cc9d69d638a4c285d1ab1ad3116e844
evalambert <145194730+evalambert@users.noreply.github.com>
2023-10-10 16:45:42 +0200

    maj

--- vite.config.js
+++ vite.config.js
@@ -15,9 +15,7 @@ export default {
     hot: true
   }
 } */
-
-
-
+/* 
 const { resolve } = require('path')
 
 const root = resolve(__dirname, 'src')
@@ -48,4 +46,37 @@ export default {
       port: 8080,
       hot: true
     }
-} 
+}  */
+
+import path from "path";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+/* Vite config */
+export default defineConfig({
+  base: "/restaurant-css-framework/",
+  root: path.resolve(__dirname, "src"),
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        //main: path.resolve(__dirname, "src/js/main.js"),
+        main: path.resolve(__dirname, "src/index.html"),
+        menu: path.resolve(__dirname, "src/menu.html"),
+        galerie1: path.resolve(__dirname, "src/galerie-1.html"),
+        galerie2: path.resolve(__dirname, "src/galerie-2.html"),
+        galerie3: path.resolve(__dirname, "src/galerie-3.html"),
+        contact: path.resolve(__dirname, "src/contact.html"),
+        restaurant: path.resolve(__dirname, "src/restaurant.html"),
+      },
+    },
+    server: {
+      port: 8080,
+    },
+  },
+});
